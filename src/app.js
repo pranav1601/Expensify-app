7f36eca420fe7a1c68afe79f171ab3fe069d9a4a
pranav1601 <pranav.agarw@gmail.com>
2020-04-21 03:19:10 +0530

    remove dummy data

--- src/app.js
+++ src/app.js
@@ -11,20 +11,6 @@ import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
 const store=configureStore();
-
-
-store.dispatch(addExpense({description:'water bill',amount: 1000}));
-store.dispatch(addExpense({description:'gas bill',createdAt: 1000}));
-store.dispatch(addExpense({description:'rent',amount: 20000}));
-
-
-
-
-const state=store.getState();
-const visibleExpenses=getVisibleExpense(state.expenses,state.filters);
-
-// console.log(visibleExpenses);
-
 const jsx=(
     <Provider store={store}>
         <AppRouter/>
