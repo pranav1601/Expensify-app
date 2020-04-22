247014868461733b526eed59393710ad42650777
pranav1601 <pranav.agarw@gmail.com>
2020-04-23 03:23:19 +0530

    format amount

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItem=({dispatch,description,amount,createdAt,id})=>(
     <div>
@@ -8,7 +9,7 @@ const ExpenseListItem=({dispatch,description,amount,createdAt,id})=>(
             <h3>{description}</h3>
         </Link>
         
-        <p>{amount}- {createdAt}</p>
+        <p>Rs.{numeral(amount).format('Rs0,0.00')}- {moment(createdAt).format('MMMM D0, YYYY')}</p>
     </div>
 );
 
